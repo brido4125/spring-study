7d8d7949cfd81405ec406f2e5b6ec616635a741d
hcs4125 <hcs4125@gmail.com>
2021-09-07 17:28:52 +0900

    상품 엔티티 수량 증가/감소 로직 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,23 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /*
+     *   재고수량 증가
+     */
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    /*
+     *   재고수량 감소
+     */
+    public void removeStock(int stockQuantity) {
+        int restStock = this.stockQuantity - stockQuantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("수량은 0보다 커야합니다.");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
