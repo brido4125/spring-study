f24fac80b80c5f8907b6a360860076a4a60c0c48
hcs4125 <hcs4125@gmail.com>
2021-09-13 14:47:18 +0900

    item 등록 구현

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -3,14 +3,13 @@ package jpabook.jpashop.controller;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Controller;
 
 import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 public class MemberForm {
-    @NotEmpty(message = "이름은 필수입니다.") private String name;
+    @NotEmpty(message = "회원명은 필수입니다.") private String name;
     private String city;
     private String street;
     private String zipcode;
