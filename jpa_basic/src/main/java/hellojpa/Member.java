fdf266d94e324d1e1211ce147b0d58c7c7719dfb
hcs4125 <hcs4125@gmail.com>
2021-09-19 21:57:43 +0900

    ddl 자동생성 설정

--- jpa_basic/src/main/java/hellojpa/Member.java
+++ jpa_basic/src/main/java/hellojpa/Member.java
@@ -10,14 +10,15 @@ import javax.persistence.Id;
 @Setter
 @Entity
 public class Member {
+    /*
+     * @Entity => 기본 생성자 필수
+     */
     public Member() {
     }
-
     public Member(Long id, String name) {
         this.id = id;
         this.name = name;
     }
-
     @Id //primary key 설정
     private Long id;
     private String name;
