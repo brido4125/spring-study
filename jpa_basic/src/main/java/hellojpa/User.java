b1100e93f205f9b699a8b24185820c944166894b
hcs4125 <hcs4125@gmail.com>
2021-09-28 15:11:17 +0900

    다대일 단방향, 양방향 설정

--- jpa_basic/src/main/java/hellojpa/User.java
+++ jpa_basic/src/main/java/hellojpa/User.java
@@ -14,9 +14,11 @@ public class User {
 
     @Column(name = "USERNAME")
     private String username;
+    //요까지만 연관관계 설정하면 단방향 연관관계
     @ManyToOne
     @JoinColumn(name = "TEAM_ID")
     private Team team;
+
 
     /*
     * 연관관계 편의 메서드 생성 => 항상 양쪽에 값을 설정하는 것이 가능!
