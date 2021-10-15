9c843540af2f9f4f62d012d29314a24986f7af84
hcs4125 <hcs4125@gmail.com>
2021-10-15 22:45:57 +0900

    add 로직 구현

--- studentsystem/src/main/java/com/example/studentsystem/service/StudentService.java
+++ studentsystem/src/main/java/com/example/studentsystem/service/StudentService.java
@@ -1,4 +1,8 @@
 package com.example.studentsystem.service;
 
+import com.example.studentsystem.model.Student;
+import org.springframework.stereotype.Service;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
 }
--- studentsystem/src/main/java/com/example/studentsystem/service/StudentServiceImpl.java
+++ studentsystem/src/main/java/com/example/studentsystem/service/StudentServiceImpl.java
@@ -1,4 +1,20 @@
 package com.example.studentsystem.service;
 
-public class StudentServiceImpl {
+import com.example.studentsystem.model.Student;
+import com.example.studentsystem.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
 }
