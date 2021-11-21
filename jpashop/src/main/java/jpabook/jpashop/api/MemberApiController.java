b1823bea7cd54dc713d25d6cc331ba9f8b1baabc
hcs4125 <hcs4125@gmail.com>
2021-11-21 23:13:32 +0900

    updateMember

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,11 +2,10 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -29,6 +28,11 @@ public class MemberApiController {
         return new CreateMemberResponse(join);
     }
 
+    @PutMapping("/api/v2/members")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id")Long id
+            , @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+    }
     @Data
     static class CreateMemberRequest{
         private String name;
@@ -41,5 +45,17 @@ public class MemberApiController {
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private String name;
+        private Long id;
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
     }
 }
