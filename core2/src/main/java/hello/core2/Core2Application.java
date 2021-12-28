f068ae42292ba5f60c3b6d492ace61c18b8fc3c0
hcs4125 <hcs4125@gmail.com>
2021-12-28 09:25:53 +0900

    feat: creating member package

--- core2/src/main/java/hello/core2/Core2Application.java
+++ core2/src/main/java/hello/core2/Core2Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Core2Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Core2Application.class, args);
 	}
