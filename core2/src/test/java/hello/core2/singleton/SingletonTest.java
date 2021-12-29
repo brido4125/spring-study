5df6ad9132981674266bb466e32329e529a4d5f6
hcs4125 <hcs4125@gmail.com>
2021-12-29 16:53:57 +0900

    feat:Singleton Container Test

--- core2/src/test/java/hello/core2/singleton/SingletonTest.java
+++ core2/src/test/java/hello/core2/singleton/SingletonTest.java
@@ -1,10 +1,13 @@
 package hello.core2.singleton;
 
 import hello.core2.AppConfig;
+import hello.core2.member.Member;
 import hello.core2.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,6 +21,17 @@ public class SingletonTest {
         MemberService memberService2 = appConfig.memberService();
 
         assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("Spring DI Container")
+    void springContainer() {
+        //호출 할 때 마다 인스터스 새롭게 생성 => memory overhead가 너무 크다
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 
     @Test
