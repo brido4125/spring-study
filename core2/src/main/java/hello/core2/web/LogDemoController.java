3757413961259d14b325b4cdd57313b37cce8059
hcs4125 <hcs4125@gmail.com>
2022-01-04 11:20:05 +0900

    feat: Error Fix with Object Provider

--- core2/src/main/java/hello/core2/web/LogDemoController.java
+++ core2/src/main/java/hello/core2/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core2.web;
 
 import hello.core2.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,11 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
         String string = request.getRequestURL().toString();
         myLogger.setRequestURL(string);
 
