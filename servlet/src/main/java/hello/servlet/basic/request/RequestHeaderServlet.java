2ebac76d1d25751fe0f5791dfbca73cd3b85b817
hcs4125 <hcs4125@gmail.com>
2022-01-10 22:32:05 +0900

    feat: RequestHeadServlet

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,7 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "requestHeaderServlet",urlPatterns = "/request-header")
+public class RequestHeaderServlet {
+}
