aeab4bb1ae0e48f0fe43ab46df8c64bc592d9ef4
hcs4125 <hcs4125@gmail.com>
2022-01-11 22:37:12 +0900

    feat: overriding service method

--- servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -1,7 +1,16 @@
 package hello.servlet.basic.request;
 
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
 @WebServlet(name = "requestHeaderServlet",urlPatterns = "/request-header")
-public class RequestHeaderServlet {
+public class RequestHeaderServlet extends HttpServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+    }
 }
