f800df4b4106dfff1cfd8e91e6bb0605cafcd681
hcs4125 <hcs4125@gmail.com>
2022-02-11 14:01:39 +0900

    update: Servlet Request Example

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet",urlPatterns = "/request-body-string")
+/*
+* x-www-form-urlencoded 방식으로 데이터가 넘어옴
+* 아래와 같은 형식으로 form 데이터가 전송됨
+* => 변수=값&변수2=값2
+* */
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+        response.getWriter().write("OK");
+    }
+}
