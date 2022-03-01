0be160bf7c342adb4d0a2782ebcb38162c2a16ef
hcs4125 <hcs4125@gmail.com>
2022-03-01 20:22:52 +0900

    feat:frontTodo

--- todo/front/src/App.js
+++ todo/front/src/App.js
@@ -1,23 +1,50 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
 import Todo from "./Todo";
-import React from "react";
-import { render } from "@testing-library/react";
+import AddTodo from "./AddTodo.js";
+import { Paper, List, Container } from "@material-ui/core";
+import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      item: { id: 0, title: "Work out", done: true },
-    };
-  }
-  render() {
-    return (
-      <div className="App">
-        <Todo item={this.state.item} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [state, setState] = useState({ items: [] });
+
+  const add = (item) => {
+    const thisItems = state.items;
+    item.id = "ID-" + thisItems.length; // key를 위한 id추가
+    item.done = false; // done 초기화
+    thisItems.push(item); // 배열에 아이템 추가
+    setState({ items: thisItems }); // 업데이트는 반드시 this.setState로 해야됨.
+    console.log("items : ", state.items);
+  };
+
+  const deleteItem = (item) => {
+    const thisItems = state.items;
+    console.log("Before Update Items : ", state.items);
+    const newItems = thisItems.filter((e) => e.id !== item.id); // 해당 id 걸러내기
+    setState({ items: newItems }, () => {
+      // 디버깅 콜백
+      console.log("Update Items : ", state.items);
+    });
+  };
+
+  var todoItems = state.items.length > 0 && (
+    <Paper style={{ margin: 16 }}>
+      <List>
+        {state.items.map((item, idx) => (
+          <Todo item={item} key={item.id} deleteItem={deleteItem} />
+        ))}
+      </List>
+    </Paper>
+  );
+
+  // 3. props로 넘겨주기å
+  return (
+    <div className="App">
+      <Container maxWidth="md">
+        <AddTodo add={add} />
+        <div className="TodoList">{todoItems}</div>
+      </Container>
+    </div>
+  );
+};
 
 export default App;
--- todo/front/src/Todo.js
+++ todo/front/src/Todo.js
@@ -1,25 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  ListItem,
+  ListItemText,
+  InputBase,
+  Checkbox,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@material-ui/core";
 
-class Todo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      item: props.item,
-    };
-  }
-  render() {
-    return (
-      <div className="Todo">
-        <input
-          type="checkbox"
-          id={this.state.item.id}
-          name={this.state.item.id}
-          value={this.state.item.done}
+import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
+
+const Todo = (props) => {
+  const [state, setState] = useState({ item: props.item, readOnly: true });
+  const deleteItem = props.deleteItem;
+
+  const deleteEventHandler = () => {
+    deleteItem(state.item);
+  };
+
+  const offReadOnlyMode = () => {
+    console.log("Event!", state.readOnly);
+    setState({ readOnly: false }, () => {
+      console.log("ReadOnly? ", state.readOnly);
+    });
+  };
+
+  const enterKeyEventHandler = (e) => {
+    if (e.key === "Enter") {
+      setState({ readOnly: true });
+    }
+  };
+
+  const editEventHandler = (e) => {
+    const thisItem = state.item;
+    thisItem.title = e.target.value;
+    setState({ item: thisItem });
+  };
+
+  const checkboxEventHandler = (e) => {
+    const thisItem = state.item;
+    thisItem.done = !thisItem.done;
+    setState({ item: thisItem });
+  };
+
+  const item = state.item;
+  return (
+    <ListItem>
+      <Checkbox checked={item.done} onChange={checkboxEventHandler} />
+      <ListItemText>
+        <InputBase
+          inputProps={{
+            "aria-label": "naked",
+            readOnly: state.readOnly,
+          }}
+          type="text"
+          id={item.id}
+          name={item.id}
+          value={item.title}
+          fullWidth={true}
+          onClick={offReadOnlyMode}
+          onChange={editEventHandler}
+          onKeyPress={enterKeyEventHandler}
         />
-        <label id={this.state.item.id}>{this.state.item.title}</label>
-      </div>
-    );
-  }
-}
+      </ListItemText>
+
+      <ListItemSecondaryAction>
+        <IconButton aria-label="Delete Todo" onClick={deleteEventHandler}>
+          <DeleteOutlined />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
+};
 
 export default Todo;
