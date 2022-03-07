6c13e30c8eace78bb1f20590bf8e7d90e5e9de7f
hcs4125 <hcs4125@gmail.com>
2022-03-07 16:08:51 +0900

    update:Logger

---
+++ springmvc/src/main/java/brido/springmvc/basic/LogTestController.java
@@ -0,0 +1,31 @@
+package brido.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * RestController => String return
+ * Controller => view name return
+ */
+@Slf4j
+@RestController
+public class LogTestController {
+    //private final Logger logger = LoggerFactory.getLogger(LogTestController.class);
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "brido";
+
+        //log.trace("trace log = " + name); => 불필요한 String 더하기 연산 발생해서 메모리 낭비 발생
+        log.trace("trace log = {}",name);
+        log.debug("debug log = {}",name);
+        log.info("info log is={}", name);
+        log.warn("warn log = {}",name);
+        log.error("error log = {}",name);
+        return "test";
+    }
+}
