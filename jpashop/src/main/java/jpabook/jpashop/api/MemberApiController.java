23d8fbf7cb8c4ce918eb6d1ce3be4aa87aa50ec8
hcs4125 <hcs4125@gmail.com>
2022-04-14 17:57:41 +0900

    update:Basic api

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,14 +8,42 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers
+                .stream().map(m -> new MemberDto(m.getName())).
+                collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
+
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
@@ -28,10 +56,13 @@ public class MemberApiController {
         return new CreateMemberResponse(join);
     }
 
-    @PutMapping("/api/v2/members")
-    public UpdateMemberResponse updateMemberV2(@PathVariable("id")Long id
-            , @RequestBody @Valid UpdateMemberRequest request) {
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id")Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
         memberService.update(id, request.getName());
+        Member one = memberService.findOne(id);
+        return new UpdateMemberResponse(one.getId(), one.getName());
     }
     @Data
     static class CreateMemberRequest{
@@ -50,8 +81,8 @@ public class MemberApiController {
     @Data
     @AllArgsConstructor
     static class UpdateMemberResponse{
-        private String name;
         private Long id;
+        private String name;
     }
 
     @Data
