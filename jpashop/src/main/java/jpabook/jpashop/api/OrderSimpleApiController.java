eaa48599c023094ac8307a87887be3499b120631
hcs4125 <hcs4125@gmail.com>
2022-04-24 15:58:15 +0900

    update:orders V3

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -47,6 +47,12 @@ public class OrderSimpleApiController {
         return orders.stream().map(SimpleOrderDto::new).collect(Collectors.toList());
     }
 
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        return orders.stream().map(SimpleOrderDto::new).collect(Collectors.toList());
+    }
+
     @Data
     static class SimpleOrderDto{
         private Long orderId;
