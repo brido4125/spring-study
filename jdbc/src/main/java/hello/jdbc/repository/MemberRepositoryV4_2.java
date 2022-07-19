42fc4ff11d5e6f49c1a74ce54bebbe1098ca0e35
hcs4125 <hcs4125@gmail.com>
2022-07-19 20:48:56 +0900

    feat:MemberRepositoryV4_2

---
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV4_2.java
@@ -0,0 +1,130 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.donmain.Member;
+import hello.jdbc.repository.ex.MyDbException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/*
+* SQL ExceptionTranslator 추가
+* */
+@Slf4j
+public class MemberRepositoryV4_2 implements MemberRepository{
+
+    private final DataSource dataSource;
+
+    private final SQLExceptionTranslator translator;
+
+    public MemberRepositoryV4_2(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.translator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
+    }
+
+    @Override
+    public Member save(Member member){
+        String sql = "insert into member(member_id,money) values (?, ?)";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            throw translator.translate("save", sql, e);
+        } finally {
+            closing(con,pstmt,null);
+        }
+    }
+
+    public Member findById(String memberId){
+        String sql = "select * from member where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("Member not founded memberId = " + memberId);
+            }
+        } catch (SQLException e) {
+            throw translator.translate("findById", sql, e);
+
+        } finally {
+            closing(con,pstmt,rs);
+        }
+
+    }
+
+    public void update(String memberId, int money){
+        String sql = "update member set money=? where member_id=?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();//쿼리를 실행하고나서 영향을 받은 row 수을 반환
+            log.info("resultSize = {}", resultSize);
+        } catch (SQLException e) {
+            throw translator.translate("update", sql, e);
+
+        } finally {
+            closing(con, pstmt, null);
+        }
+    }
+
+    public void delete(String memberId){
+        String sql = "delete from member where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw translator.translate("delete", sql, e);
+
+        } finally {
+            closing(con, pstmt, null);
+        }
+    }
+
+    private void closing(Connection con, Statement stmt, ResultSet resultSet) {
+        JdbcUtils.closeResultSet(resultSet);
+        JdbcUtils.closeStatement(stmt);
+        //트랜잭션 동기화를 사용하려면 DataSourceUtils 사용해야함
+        DataSourceUtils.releaseConnection(con,dataSource);
+    }
+
+    private Connection getConnection() throws SQLException {
+        //앞서 Connection을 Parameter로 넘겨서 유지하는 방식 대신
+        // -> 트랜잭션 동기화 사용하려면 DataSourceUtils 사용
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+        log.info("get connection = {}, class = {}", connection, connection.getClass());
+        return connection;
+    }
+
+}
