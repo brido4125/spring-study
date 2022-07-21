daa1546f69fb63aa5923284350d08d61e7238da8
hcs4125 <hcs4125@gmail.com>
2022-07-21 23:03:36 +0900

    update:javascript inline

--- thymleaf-basic/src/main/java/hello/thymleaf/basic/BasicController.java
+++ thymleaf-basic/src/main/java/hello/thymleaf/basic/BasicController.java
@@ -138,4 +138,11 @@ public class BasicController {
         addUsers(model);
         return "basic/block";
     }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 22));
+        addUsers(model);
+        return "basic/javascript";
+    }
 }
