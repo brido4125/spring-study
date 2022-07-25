a95d2b7a7aabe3f5cc4eb0e5cfda359d7408433b
hcs4125 <hcs4125@gmail.com>
2022-07-25 18:27:26 +0900

    feat:Item field update

--- form/src/main/java/hello/itemservice/domain/item/Item.java
+++ form/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,6 +1,9 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import lombok.experimental.Delegate;
+
+import java.util.List;
 
 @Data
 public class Item {
@@ -9,6 +12,11 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    private boolean open;//판매여부
+    private List<String> regions; // 등록지역
+    private ItemType itemType;
+    private DeliveryCode deliveryCode;
 
     public Item() {
     }
