780f543b63b3fcc07c26d0f93ee37651283b04c4
hcs4125 <hcs4125@gmail.com>
2022-08-10 21:49:37 +0900

    feat:ControllerV1 - 클래식한 검증

---
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -0,0 +1,107 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Slf4j
+@Controller
+@RequestMapping("/validation/v1/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV1 {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation/v1/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v1/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation/v1/addForm";
+    }
+
+
+    /*
+    * @ModelAttribute로 Item을 model에 심어주었기 때문에
+    * 검증에서 걸러져서 addForm으로 반환 될때, 기존에 입력했던 Item의 값이 유지된채로 Form이 반환됩니다.!
+    * */
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes,Model model) {
+
+        //검증 오류 결과를 보관하는 객체가 필요
+        Map<String, String> errors = new HashMap<>();
+
+        //검증 로직 만들기
+        if (!StringUtils.hasText(item.getItemName())) { //itemName이 들어오지 않으면
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "가격은 1,000원 ~ 1,000,000 사이의 값이어야 합니다.");
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() >= 9999 || item.getQuantity() < 0) {
+            errors.put("quantity", "수량은 0보다 크고 9999개 보다는 커야 합니다.");
+        }
+
+        //특정 필드값 검증이 아닌 복합 필드 검증 (가격 + 수량)
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.put("globalError", "가격 * 주문 수량은 10,000이상 이어야합니다. 현재 값 = " + resultPrice);
+            }
+        }
+
+        //검증에 실패하면 다시 입력 Form으로 이동
+        if (!errors.isEmpty()) { //map에 요소가 있으면,즉 에러가 있으면
+            log.info("error = {}",errors);
+            model.addAttribute("errors", errors);
+            //검증을 통과하지 못하면 입력 Form으로 보내기
+            return "validation/v1/addForm";
+        }
+
+        //성공 Logic
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v1/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v1/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v1/items/{itemId}";
+    }
+
+}
+
