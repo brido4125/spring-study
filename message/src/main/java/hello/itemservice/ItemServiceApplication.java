ea338beff691916f461eef55ed4d3123f627e193
hcs4125 <hcs4125@gmail.com>
2022-08-10 20:33:50 +0900

    update:메세지,국제화

--- message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,9 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -10,4 +13,15 @@ public class ItemServiceApplication {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+
+	/*
+	//Springboot가 알아서 빈 등록 해준다 => 주석 처리 가능
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames("messages", "errors");//resources의 messages,erros.properties를 읽어들임
+		messageSource.setDefaultEncoding("utf-8");
+		return messageSource;
+	}
+	*/
 }
