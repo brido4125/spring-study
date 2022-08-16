48134287b9c29670f419bac9dba9b4466016a6f8
hcs4125 <hcs4125@gmail.com>
2022-08-16 18:04:25 +0900

    update:Bean Validation2

--- validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
+++ validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -7,7 +7,11 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.util.Locale;
 import java.util.Set;
+
+import static java.util.Locale.ENGLISH;
+import static java.util.Locale.KOREA;
 
 public class BeanValidationTest {
 
@@ -25,6 +29,24 @@ public class BeanValidationTest {
         for (ConstraintViolation<Item> itemConstraintViolation : validate) {
             System.out.println("itemConstraintViolation.getMessage() = " + itemConstraintViolation.getMessage());
         }
+    }
+
+    /*
+    * "널이어서는 안됩니다" => Locale Default가 KOREA면 자동으로 등록됨
+    * javax.validation.constraints.NotNull.message
+    * */
+    @Test
+    void nullErrorMsg() {
+        Locale.setDefault(KOREA);
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item();
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+        }
+
     }
 
 }
