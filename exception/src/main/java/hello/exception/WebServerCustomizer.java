441cc207f877aaa45d52c4e24dce7c9e317402d8
hcs4125 <hcs4125@gmail.com>
2022-08-25 20:24:56 +0900

    update:에러페이지 커스터마이징

---
+++ exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,24 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    /*
+    * WebServer를 커스터 마이징 하는 설정
+    * 기본적으로는 톰캣 서버 커스터마이징한다고 생각하면 됩니다.
+    * */
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+
+    }
+}
