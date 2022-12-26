7859af5110f38a7e1998e14a70c6dcc3c23ea114
brido4125 <hcs4125@gmail.com>
2022-12-26 23:53:30 +0900

    update

--- core3/src/main/java/hello/core3/AutoAppConfig.java
+++ core3/src/main/java/hello/core3/AutoAppConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
+/*
+* basePackages를 지정하지 않는 경우 -> hello.core3 패키지 하위 디렉토리로 Scan 진행
+* */
 @ComponentScan(
+        basePackages = "hello.core3",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //기존의 AppConfig Bean 제외
 )
 public class AutoAppConfig {
