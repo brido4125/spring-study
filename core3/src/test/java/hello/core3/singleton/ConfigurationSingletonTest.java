91d66a2fd082c39a9c9b98854d37fb9c10c3d0b9
brido4125 <hcs4125@gmail.com>
2023-04-15 16:56:26 +0900

    update:CGLIB Test code

--- core3/src/test/java/hello/core3/singleton/ConfigurationSingletonTest.java
+++ core3/src/test/java/hello/core3/singleton/ConfigurationSingletonTest.java
@@ -24,4 +24,22 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberRepository1).isEqualTo(memberRepository2);
         Assertions.assertThat(memberRepository).isEqualTo(memberRepository2);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        /*
+        * AppConfig -> AppConfig$$EnhancerBySpringCGLIB으로 바뀜
+        * AppConfig를 상속하는게 AppConfigCGLIB
+        * 부모 타입으로 조회하면 자식 타입의 Bean들도 전부 조회됨
+        * CGLIB에서 빈이 이미 컨테이너에 등록되어 있으면 해당 Bean 리턴
+        * 그렇지 않을 경우에만 실제 빈을 생성해서 리턴(위에서는 MemberRepository)
+        * */
+        System.out.println("bean.getClass() = " + bean.getClass());
+
+        /*
+        * @Configuration을 제외하면 Singleton이 깨지면서 요청마다 새로운 객체를 생성한다
+        * */
+    }
 }
