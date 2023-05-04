da45c90c02156d073069ef7cd3c6fd3cebd51555
brido4125 <hcs4125@gmail.com>
2023-05-04 20:36:58 +0900

    update

---
+++ advanced/src/main/java/hello/advanced/trace/template/AbstractTemplate.java
@@ -0,0 +1,26 @@
+package hello.advanced.trace.template;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+
+public abstract class AbstractTemplate<T> {
+    private final LogTrace logTrace;
+
+    public AbstractTemplate(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    public T excute(String message) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin(message);
+            T result = call();
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+    protected abstract T call();
+}
