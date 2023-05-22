47895c987e3383df8b91b4ce476f0d5744603c46
brido4125 <hcs4125@gmail.com>
2023-05-22 23:45:16 +0900

    update:카페키오스크 필수 클래스 작성

---
+++ cafekiosk/src/main/java/sample/cafekiosk/unit/beverage/Americano.java
@@ -0,0 +1,12 @@
+package sample.cafekiosk.unit.beverage;
+
+public class Americano implements Beverage {
+
+    public int getPrice() {
+        return 4000;
+    }
+
+    public String getName() {
+        return "Americano";
+    }
+}
---
+++ cafekiosk/src/main/java/sample/cafekiosk/unit/order/Order.java
@@ -0,0 +1,15 @@
+package sample.cafekiosk.unit.order;
+
+import lombok.RequiredArgsConstructor;
+import sample.cafekiosk.unit.beverage.Beverage;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class Order {
+
+    private final LocalDateTime localDateTime;
+
+    private final List<Beverage> beverages;
+}
