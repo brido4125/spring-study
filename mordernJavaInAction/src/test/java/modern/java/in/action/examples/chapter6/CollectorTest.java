e6baba27117c4edd9da0c4ebda3f53cb783bd5c1
brido4125 <hcs4125@gmail.com>
2023-06-10 17:05:49 +0900

    update:분할 테스트 코드

--- mordernJavaInAction/src/test/java/modern/java/in/action/examples/chapter6/CollectorTest.java
+++ mordernJavaInAction/src/test/java/modern/java/in/action/examples/chapter6/CollectorTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -90,6 +91,103 @@ public class CollectorTest {
             System.out.println("entry.getKey() = " + entry.getKey());
             entry.getValue().stream().map(Dish::getName).forEach(System.out::println);
         }
+    }
+
+    @Test
+    void multipleGroupBy() {
+        Map<Dish.Type, Map<CaloricLevel, List<Dish>>> listMap = menu.stream()
+                .collect(Collectors.groupingBy(Dish::getType,
+                        Collectors.groupingBy(dish -> {
+                            if (dish.getCalories() <= 400) return CaloricLevel.DIET;
+                            else if (dish.getCalories() <= 700) return CaloricLevel.NORMAL;
+                            else return CaloricLevel.FAT;
+                        })));
+
+        for (Map.Entry<Dish.Type, Map<CaloricLevel, List<Dish>>> entry : listMap.entrySet()) {
+            System.out.println("entry.getKey() = " + entry.getKey());
+            for (Map.Entry<CaloricLevel, List<Dish>> entry1 : entry.getValue().entrySet()) {
+                System.out.println("entry1.getKey() = " + entry1.getKey());
+                entry1.getValue().stream().map(Dish::getName).forEach(System.out::println);
+            }
+            System.out.println("==========================");
+        }
+    }
+
+    @Test
+    void reverseGroupBy() {
+        Map<CaloricLevel, Map<Dish.Type, List<Dish>>> reverse = menu.stream()
+                .collect(Collectors.groupingBy(dish -> {
+                    if (dish.getCalories() <= 400) return CaloricLevel.DIET;
+                    else if (dish.getCalories() <= 700) return CaloricLevel.NORMAL;
+                    else return CaloricLevel.FAT;
+                }, Collectors.groupingBy(Dish::getType)));
+
+        for (Map.Entry<CaloricLevel, Map<Dish.Type, List<Dish>>> entry : reverse.entrySet()) {
+            System.out.println("entry.getKey() = " + entry.getKey());
+            for (Map.Entry<Dish.Type, List<Dish>> entry1 : entry.getValue().entrySet()) {
+                System.out.println("entry1.getKey() = " + entry1.getKey());
+                entry1.getValue().stream().map(Dish::getName).forEach(System.out::println);
+            }
+            System.out.println("==========================");
+        }
+    }
+
+    @Test
+    void partitioning() {
+        Map<Boolean, List<Dish>> vegi = menu.stream()
+                .collect(Collectors.partitioningBy(Dish::isVegetarian));
+
+        vegi.get(true).stream().map(Dish::getName).forEach(System.out::println);
+    }
+
+    @Test
+    void partitioningWithType() {
+        Map<Boolean, Map<Dish.Type, List<Dish>>> vegi = menu.stream()
+                .collect(Collectors.partitioningBy(Dish::isVegetarian, Collectors.groupingBy(Dish::getType)));
+
+        for (Map.Entry<Boolean, Map<Dish.Type, List<Dish>>> entry : vegi.entrySet()) {
+            System.out.println("entry.getKey() = " + entry.getKey());
+            for (Map.Entry<Dish.Type, List<Dish>> entry1 : entry.getValue().entrySet()) {
+                System.out.println("entry1.getKey() = " + entry1.getKey());
+                entry1.getValue().stream().map(Dish::getName).forEach(System.out::println);
+            }
+            System.out.println("==========================");
+        }
+    }
+
+    @Test
+    void partitioningWithMaxCalories() {
+        Map<Boolean, Dish> max = menu.stream()
+                .collect(Collectors.partitioningBy(Dish::isVegetarian,
+                        Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparingInt(Dish::getCalories)),
+                                Optional::get)));
+
+        for (Map.Entry<Boolean, Dish> entry : max.entrySet()) {
+            System.out.println("entry.getKey() = " + entry.getKey());
+            System.out.println("entry.getValue() = " + entry.getValue().getName());
+        }
+    }
+
+
+    @Test
+    void predicatePrimeNumberWithCollector() {
+        Map<Boolean, Long> primeNum = IntStream.rangeClosed(2, 100)
+                .boxed()
+                .collect(Collectors.partitioningBy(this::isPrime, Collectors.counting()));
+
+        for (Map.Entry<Boolean, Long> entry : primeNum.entrySet()) {
+            System.out.println("entry.getKey() = " + entry.getKey());
+            System.out.println("entry.getValue() = " + entry.getValue());
+        }
+    }
+
+    /*
+    * 소수 판별 할 대상을 제곱근 이하의 수로 제한하는 방법
+    * */
+    private boolean isPrime(int candidate) {
+        int candidateRoot = (int) Math.sqrt((double) candidate);
+        return IntStream.rangeClosed(2, candidateRoot)
+                .noneMatch(i -> candidate % i == 0);
     }
 
 }
