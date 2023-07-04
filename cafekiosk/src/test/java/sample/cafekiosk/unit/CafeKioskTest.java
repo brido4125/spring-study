d280e08ab88e9c5654ccc2abd3d7b9a6fe679acb
brido4125 <hcs4125@gmail.com>
2023-07-04 23:08:21 +0900

    update:단위 테스트

--- cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -1,15 +1,36 @@
 package sample.cafekiosk.unit;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 
 public class CafeKioskTest {
 
     @Test
-    void add() {
+    void menuTest() {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.add(new Americano());
         System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
         System.out.println(">>> 총 가격 : " + cafeKiosk.getTotalPrice());
+    }
+
+    @Test
+    void add() {
+        CafeKiosk kiosk = new CafeKiosk();
+        kiosk.add(new Americano());
+
+        Assertions.assertThat(kiosk.getBeverages().size()).isEqualTo(1);
+        Assertions.assertThat(kiosk.getTotalPrice()).isEqualTo(4000);
+    }
+
+    @Test
+    void remove() {
+        CafeKiosk kiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        kiosk.add(americano);
+
+        kiosk.remove(americano);
+
+        Assertions.assertThat(kiosk.getBeverages().size()).isEqualTo(0);
     }
 }
