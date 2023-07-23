2cc1f702f47e78ba2e23dee55ad5e09f802aec3a
brido4125 <hcs4125@gmail.com>
2023-07-23 18:47:41 +0900

    update: comments

--- mordernJavaInAction/src/test/java/modern/java/in/action/examples/chapter7/ForkJoinSumCalc.java
+++ mordernJavaInAction/src/test/java/modern/java/in/action/examples/chapter7/ForkJoinSumCalc.java
@@ -4,6 +4,9 @@ import java.util.concurrent.RecursiveTask;
 
 public class ForkJoinSumCalc extends RecursiveTask<Long> {
     private final long[] numbers;
+    /*
+    * start,end -> 연산을 시작할 numbers 배열의 시작과 끝 인덱스
+    * */
     private final int start;
     private final int end;
     public static final long THRESHOLD = 10_000;
@@ -25,12 +28,16 @@ public class ForkJoinSumCalc extends RecursiveTask<Long> {
         if (length <= THRESHOLD) {
             return computeSequentially();
         }
+        /*
+        * Task split to half chuck.
+        * */
         ForkJoinSumCalc leftTask = new ForkJoinSumCalc(numbers, start, start + length / 2);
+        //start the Task asynchronously, specifically push this job to queue.
         leftTask.fork();
 
         ForkJoinSumCalc rightTask = new ForkJoinSumCalc(numbers, start + length / 2, end);
-        Long compute = rightTask.compute();
-        Long join = leftTask.join();
+        Long compute = rightTask.compute();//main thread will compute rightTask.
+        Long join = leftTask.join();//get result of leftTask.
         return compute + join;
     }
 
