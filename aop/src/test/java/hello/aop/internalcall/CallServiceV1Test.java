75f729798f3a506e546d5e54a06686dcc34ae5c1
brido4125 <hcs4125@gmail.com>
2023-08-29 21:39:28 +0900

    update:Setter 주입으로 해결

--- aop/src/test/java/hello/aop/internalcall/CallServiceV1Test.java
+++ aop/src/test/java/hello/aop/internalcall/CallServiceV1Test.java
@@ -2,28 +2,28 @@ package hello.aop.internalcall;
 
 import hello.aop.internalcall.aop.CallLogAspect;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 
-@Slf4j
 @Import(CallLogAspect.class)
 @SpringBootTest
-class CallServiceV0Test {
+class CallServiceV1Test {
 
     @Autowired
-    CallServiceV0 callServiceV0;
+    CallServiceV1 callServiceV1;
 
     @Test
     void external() {
-        callServiceV0.external();
+        callServiceV1.external();
     }
 
     @Test
     void internal() {
-        callServiceV0.internal();
+        callServiceV1.internal();
     }
 
 }
