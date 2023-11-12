16b710fee2aa6260c4660c9d6c85ae92977b80bb
brido4125 <hcs4125@gmail.com>
2023-11-12 14:40:50 +0900

    update: 1.6

--- springbook/src/main/java/vol1/springbook/user/dao/UserDao.java
+++ springbook/src/main/java/vol1/springbook/user/dao/UserDao.java
@@ -8,11 +8,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
+    private static UserDao INSTANCE;
 
     private final ConnectionMaker simpleConnectionMaker;
 
-    public UserDao(ConnectionMaker connectionMaker) {
+    private UserDao(ConnectionMaker connectionMaker) {
         this.simpleConnectionMaker = connectionMaker;
+    }
+
+    public static synchronized UserDao getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new UserDao(??);
+        }
+        return INSTANCE;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
