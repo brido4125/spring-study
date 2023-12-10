d0566efbf1a33fb0bdef5d3e7112e4e5cea6c628
brido4125 <hcs4125@gmail.com>
2023-12-10 17:18:32 +0900

    update

--- server/src/main/java/hello/container/AppInitV3Spring.java
+++ server/src/main/java/hello/container/AppInitV3Spring.java
@@ -1,2 +1,29 @@
-package hello.container;public class AppInitV3Spring {
+package hello.container;
+
+import hello.spring.HelloConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AppInitV3Spring implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        System.out.println("AppInitV3Spring.onStartup");
+
+        //스프링 컨테이너 생성
+        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
+        ac.register(HelloConfig.class);
+
+        //스프링 MVC 디스패처 서블릿 생성, 스프링 컨테이너 연결
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(ac);
+
+        //Dispatcher servlet을 서블릿 컨테이너에 등록
+        ServletRegistration.Dynamic dispatcherV3 = servletContext.addServlet("dispatcherV3", dispatcherServlet);
+
+        // 모든요청들은 dispatcherV3를 거치도록 설정
+        dispatcherV3.addMapping("/");
+    }
 }
