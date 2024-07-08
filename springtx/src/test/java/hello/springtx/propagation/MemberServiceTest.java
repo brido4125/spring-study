58b8941b39b6c4ac68b53d1204dcd1854530b69d
brido4125 <hcs4125@gmail.com>
2024-07-08 14:19:56 +0900

    update

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
@@ -36,4 +35,52 @@ class MemberServiceTest {
         Assertions.assertTrue(logRepository.find(username).isPresent());
     }
 
+    /**
+     * memberService        @Transactional: OFF
+     * memberRepository     @Transactional: ON
+     * logRepository        @Transactional: ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        String username = "로그예외_outerTxOff_fail";
+
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> {
+            memberService.joinV1(username);
+        }).isInstanceOf(RuntimeException.class);
+
+
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertFalse(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService        @Transactional: On
+     */
+    @Test
+    void singleTx() {
+        String username = "outerTxOff_success";
+
+        memberService.joinV1(username);
+
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService        @Transactional: ON
+     * memberRepository     @Transactional: ON
+     * logRepository        @Transactional: ON Exception
+     */
+    @Test
+    void outerTxOn_fail() {
+        String username = "로그예외_outerTxOn_fail";
+
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> {
+            memberService.joinV1(username);
+        }).isInstanceOf(RuntimeException.class);
+
+
+        Assertions.assertTrue(memberRepository.find(username).isEmpty());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+    }
 }
