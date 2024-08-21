e8c1916d81d63e5f6afdf500fe718742da95c2cb
brido4125 <hcs4125@gmail.com>
2024-08-21 21:32:26 +0900

    project settings

---
+++ hashring/src/main/java/brido/com/hashring/HashringApplication.java
@@ -0,0 +1,13 @@
+package brido.com.hashring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashringApplication.class, args);
+	}
+
+}
---
+++ hashring/src/test/java/brido/com/hashring/HashringApplicationTests.java
@@ -0,0 +1,13 @@
+package brido.com.hashring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
