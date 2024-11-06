19296773ef898d72c6c551fe39dcd057f5dc3371
brido4125 <hcs4125@gmail.com>
2024-11-06 14:55:57 +0900

    ouath2-session

---
+++ oauth2-session/src/main/java/org/brido/oauth2session/Oauth2SessionApplication.java
@@ -0,0 +1,13 @@
+package org.brido.oauth2session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SessionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Oauth2SessionApplication.class, args);
+  }
+
+}
---
+++ oauth2-session/src/test/java/org/brido/oauth2session/Oauth2SessionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brido.oauth2session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SessionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
